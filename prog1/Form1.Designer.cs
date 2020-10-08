2fdc531efc9969fd81dfdd426adbb3b1b00b48ed
Михаил Матвеев <miha1910m@list.ru>
2020-10-08 09:36:23 +0300

    Русский

--- prog1/Form1.Designer.cs
+++ prog1/Form1.Designer.cs
@@ -292,6 +292,7 @@
             this.linkLabel1.Name = "linkLabel1";
             this.linkLabel1.Size = new System.Drawing.Size(38, 13);
             this.linkLabel1.TabIndex = 0;
+            this.linkLabel1.TabStop = true;
             this.linkLabel1.Text = "Github";
             this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
             // 
